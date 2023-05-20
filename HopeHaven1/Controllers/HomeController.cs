6599cbedb27ef3b9636088b0a8c1b785b9f2aad9
Shyam Zod <shyamzod03@gmail.com>
2023-05-21 03:10:11 +0530

    Payment + Otp functionality Implemented

--- HopeHaven1/Controllers/HomeController.cs
+++ HopeHaven1/Controllers/HomeController.cs
@@ -26,43 +26,49 @@ namespace HopeHaven1.Controllers
             listOfTherapists = _context.therapists.ToList();
             return View("Home", listOfTherapists);
         }
-        public ActionResult BookSession(int Id)
+        public ActionResult BookSession(string Id=null)
         {
+            if(Id!=null)
+            {
                 Therapists therapist = new Therapists();
-                therapist = _context.therapists.Where(m => m.Id == Id).FirstOrDefault();
+                int id = Convert.ToInt32(Id);
+                therapist = _context.therapists.Where(m => m.Id == id).FirstOrDefault();
                 return View(therapist);
+            }
+            ViewBag.Otp = TempData["OTP"];
+            ViewBag.PhoneNo = TempData["PhoneNo"];
+            return View();
         }
         [HttpPost]
         public ActionResult GetOtp(string PhoneNo)
         {
             string otp = otpService.GenerateOtp(); // Generate a random OTP
-            otpService.SendOtp("+91" + PhoneNo, "123");
-            TempData["OTP"] = otp;
+            otpService.SendOtp("+91" + PhoneNo, otp);
+            Session["OTP"] = otp;
+            Session["PhoneNo"] = PhoneNo;
+            TempData["OTP"] = PhoneNo;
             TempData["PhoneNo"] = PhoneNo;
             return RedirectToAction("BookSession");
         }
 
         [HttpPost]
-        public ActionResult VerifyOtp(string enteredOtp, string validateOTP=null)
+        public ActionResult VerifyOtp(string enteredOtp,string PhoneNo)
         {
             bool isValidOtp = false;
-            if (enteredOtp == TempData["OTP"])
+            if (enteredOtp.Equals(Session["OTP"]) && PhoneNo.Equals(Session["PhoneNo"]))
             {
                 isValidOtp = true;
             }
 
             if (isValidOtp)
             {
-                // OTP is valid, proceed with the desired action
-                return RedirectToAction("Welcome");
+                return RedirectToAction("Index", "Payment");
             }
             else
             {
-                // OTP is invalid, show an error message
                 ModelState.AddModelError("", "Invalid OTP");
-                return View("VerifyOtp");
+                return View("BookSession");
             }
-            return View("Home");
         }
     }
 }
---
+++ HopeHaven1/Controllers/PaymentController.cs
@@ -0,0 +1,46 @@
+﻿using Razorpay.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HopeHaven1.Controllers
+{
+    public class PaymentController : Controller
+    {
+        private readonly string razorpayKeyId = System.Configuration.ConfigurationManager.AppSettings["RazorpayKeyId"];
+        private readonly string razorpayKeySecret = System.Configuration.ConfigurationManager.AppSettings["RazorpayKeySecret"];
+
+        public ActionResult Index()
+        {
+            ViewBag.RazorpayKeyId = razorpayKeyId;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Charge(string razorpayPaymentId, string razorpayOrderId, string razorpaySignature)
+        {
+            var client = new RazorpayClient(razorpayKeyId, razorpayKeySecret);
+            Dictionary<string, string> attributes = new Dictionary<string, string>()
+        {
+            { "razorpay_payment_id", razorpayPaymentId },
+            { "razorpay_order_id", razorpayOrderId },
+            { "razorpay_signature", razorpaySignature }
+        };
+
+            var payment = client.Payment.Fetch(razorpayPaymentId);
+
+            if (payment != null && payment["status"] == "captured")
+            {
+                // Payment succeeded
+                return View("Success");
+            }
+            else
+            {
+                // Payment failed
+                return View("Failure");
+            }
+        }
+    }
+}
